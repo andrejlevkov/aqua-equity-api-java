6c687236503603d823505fb3bf334c9813cc4431
andrejlevkov <andrejlevkov@gmail.com>
2026-05-09 13:54:02 +0200

    Fix CORS

--- src/main/java/com/aqua_equity/aqua_equity_api/config/CorsConfig.java
+++ src/main/java/com/aqua_equity/aqua_equity_api/config/CorsConfig.java
@@ -7,16 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("*")
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "https://aqua-equity.vercel.app/")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
